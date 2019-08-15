6ee3c4807f5706a84e56880a8f15a236d35a3a7c
christian <christiancleber@gmail.com>
2019-08-15 11:53:40 -0400

    x

--- baseline_model.py
+++ baseline_model.py
@@ -166,7 +166,10 @@ def mse(pred,y):
     y = np.expm1(y)
     return np.square(pred - y).mean()
 
-score = make_scorer(mse, greater_is_better=False)
+def mse_(pred,y):
+    return np.square(pred - y).mean()
+
+score = make_scorer(mse_, greater_is_better=False)
 
 #train total
 train = pd.read_csv('train.csv', sep=',')
@@ -175,6 +178,7 @@ train['description'].fillna(' ',inplace=True)
 train = train[(train['num_votes'] < 50)]
 #train = train[train['created_time'] > '2013-01-01 00:00:00']
 #train = train.dropna(subset=['source'])
+train.reset_index(inplace=True)
 print(train.info())
 
 #train reduced
@@ -185,12 +189,12 @@ print(train.info())
 #train baseline
 train_baseline = train[~train['tag_type'].isna()]
 print(train_baseline.info())
-#train = train_baseline
+train = train_baseline
 
 
 #train['location'] = [[x1,x2] for x1,x2 in zip(train['latitude'].apply(lambda x:round(x,3)),train['longitude'].apply(lambda x:round(x,3)))]
 
-####################################  Baseline ########################################
+####################################  Baseline 1 ########################################
 # Strategy: for the 24% of the data that has issue_type populated, predict the average num_votes by neighborhood
 
 # train_baseline['lat_neighbor'] = train_baseline['latitude'].apply(lambda x: round(x,3))
@@ -217,7 +221,7 @@ description = train['description']
 summary = train['summary']
 description = description.apply(lambda x:  ' '.join(gensim.utils.simple_preprocess(x)))
 summary = summary.apply(lambda x:  ' '.join(gensim.utils.simple_preprocess(x)))
-bow = CountVectorizer(max_features=10000, binary=True, max_df=0.5,ngram_range=(1,2))
+bow = CountVectorizer(max_features=5000, binary=True, max_df=0.5,ngram_range=(1,2))
 #bow = bow.fit(description+summary)
 description = bow.fit_transform(description)
 bow.vocabulary_ = None
@@ -243,19 +247,42 @@ longitude = train['longitude'].apply(lambda x: np.round(x,3))
 #Location not scaled
 #location = np.stack((latitude,longitude),axis=-1)
 
+####################################  Baseline 2 ########################################
+
+# train.loc[:,'latitude'] = train['latitude'].apply(lambda x: round(x,3))
+# train.loc[:,'longitude'] = train['longitude'].apply(lambda x: round(x,3))
+# # MSE 0.62
+# votes_avg = train[['latitude','longitude','num_votes']].groupby(['latitude','longitude']).mean() # 0.62
+#
+# pred = list()
+#
+# for index,sample in train.iterrows():
+#      pred.append(votes_avg.loc[(sample['latitude'],sample['longitude']),['num_votes']][0])
+#
+# #print("\n Baseline RMSLE: %f " % rmsle(pred, train_baseline['num_votes']))
+# print("\n Baseline MSE: %f " % np.mean(np.square(pred - train['num_votes'])))
+# print("\n")
+
+########################################################################################
+
+
+#onehot = np.stack((train['tag_type'],latitude,longitude),axis=-1)
+#onehot = np.stack((latitude,longitude),axis=-1)
 
 onehot = np.stack((train['hour'],train['dayofweek'],train['year'],latitude,longitude),axis=-1)
 from sklearn.preprocessing import OneHotEncoder
-dummies = OneHotEncoder(handle_unknown='ignore')
+dummies = OneHotEncoder(categories='auto')
 onehot = dummies.fit_transform(onehot)
 onehot = scipy.sparse.csr_matrix(onehot)
 
 
-#text_fields = scipy.sparse.hstack([description,summary])
-text_fields = scipy.sparse.hstack([description,summary,onehot])
+#text_fields = scipy.sparse.hstack([description,summary],format='csr')
+#text_fields = onehot
+text_fields = scipy.sparse.hstack([description,summary,onehot],format='csr')
+print("Dimension of text_fields: %s" % str(text_fields.get_shape()))
 
-y_train = train['num_votes'].apply(lambda x: np.log1p(x))
-#y_train = train['num_votes']
+#y_train = train['num_votes'].apply(lambda x: np.log1p(x))
+y_train = train['num_votes']
 
 
 
@@ -268,11 +295,17 @@ y_train = train['num_votes'].apply(lambda x: np.log1p(x))
 #                                 normalize=False, precompute='auto', max_iter=1000, tol=0.0001, cv=5, copy_X=True,
 #                                 verbose=0, n_jobs=1, positive=False, random_state=666, selection='cyclic')
 
-# regressor = sklearn.linear_model.RidgeCV(alphas=(0.001, 0.01, 0.1, 1.0, 10.0), fit_intercept=True, normalize=False, scoring=None, cv=5, gcv_mode=None, store_cv_values=False)
-# regressor.fit(text_fields,y_train)
-# alpha = regressor.alpha_
-# print(alpha)
-# regressor = sklearn.linear_model.Ridge(alpha=alpha, fit_intercept=True, normalize=False, copy_X=True, max_iter=None, tol=0.001, solver='auto', random_state=999)
+#regressor = sklearn.linear_model.RidgeCV(alphas=(40.0,45.0,50.0,60.0,70.0), fit_intercept=True, normalize=False, scoring=None, cv=5, gcv_mode=None, store_cv_values=False)
+#regressor.fit(text_fields,y_train)
+#alpha = regressor.alpha_
+#print(alpha)#10.0
+#regressor = sklearn.linear_model.Ridge(alpha=40.0, fit_intercept=True, normalize=False, copy_X=True, max_iter=None, tol=0.001, solver='auto', random_state=999)
+# Complete data
+# Without log : Part A: C = 40, MSE = 0.90, R2 = 0.34  Part B: C = 40, MSE = 0.88, R2 = 0.35
+# With log : Part A: C = 10.0, MSE = 0.86, R2 = 0.59  Part B: C = 10.0, MSE = 0.82, R2 = 0.60
+# Baseline
+# Without log : Part A: C = 40, MSE = 2.87, R2 = 0.13  Part B: C = 40, MSE = 2.70, R2 = 0.18
+# With log : Part A: C = 10.0, MSE = 3.05, R2 = 0.08  Part B: C = 10.0, MSE = 2.80, R2 = 0.15
 
 
 #Lasso regression: is also known as L1 regularization. The penalty it applies is a sum of the absolute values of the weights.
@@ -284,36 +317,28 @@ y_train = train['num_votes'].apply(lambda x: np.log1p(x))
 #                                          selection='cyclic')
 # regressor.fit(text_fields,y_train)
 # alpha = regressor.alpha_
-# print(alpha)
+# print(alpha)#
 # regressor = sklearn.linear_model.Lasso(alpha=alpha, fit_intercept=True, normalize=False, precompute=False, copy_X=True, max_iter=1000, tol=0.0001, warm_start=False, positive=False, random_state=999, selection='cyclic')
-
-# regressor = GradientBoostingRegressor(loss='ls', learning_rate=0.1, n_estimators=100, subsample=1.0,
-#                 criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0,
-#                 max_depth=3, min_impurity_decrease=0.0, min_impurity_split=None, init=None, random_state=999,
-#                 max_features=None, alpha=0.9, verbose=0, max_leaf_nodes=None, warm_start=False, presort='auto',
-#                 validation_fraction=0.1, n_iter_no_change=None, tol=0.0001)
-
 
 
 #### SVR
-regressor = sklearn.svm.LinearSVR(epsilon=0.0, tol=0.0001, C=0.01, loss='epsilon_insensitive',
-                                 fit_intercept=True, intercept_scaling=1.0, dual=True, verbose=0,
-                                random_state=None, max_iter=5000)
-param_grid = {'C':[0.0001,0.001,0.1, 1, 10, 100]}
-gridSearch = sklearn.model_selection.GridSearchCV(regressor, param_grid, scoring=None, n_jobs=2, iid='warn', refit=False, cv='warn', verbose=0, pre_dispatch='2*n_jobs', error_score='raise-deprecating', return_train_score=False)
-gridSearch.fit(text_fields,y_train)
-C_ = gridSearch.best_params_['C']
-print(C_) #0.001
-
-# parameters= {'C': scipy.stats.expon(scale=10)}
-# gridSearch = sklearn.model_selection.RandomizedSearchCV(regressor, parameters, n_iter=50, scoring=None, n_jobs=2, iid='warn', refit=True, cv='warn', verbose=0, pre_dispatch='2*n_jobs', random_state=None, error_score='raise-deprecating', return_train_score=False)
+# regressor = sklearn.svm.LinearSVR(epsilon=0.0, tol=0.0001, C=0.01, loss='epsilon_insensitive',
+#                                  fit_intercept=True, intercept_scaling=1.0, dual=True, verbose=0,
+#                                 random_state=None, max_iter=5000)
+# param_grid = {'C':[0.0001,0.001,0.1, 1, 10, 100]}
+# gridSearch = sklearn.model_selection.GridSearchCV(regressor, param_grid, scoring=None, n_jobs=2, iid='warn', refit=False, cv='warn', verbose=0, pre_dispatch='2*n_jobs', error_score='raise-deprecating', return_train_score=False)
 # gridSearch.fit(text_fields,y_train)
 # C_ = gridSearch.best_params_['C']
-# print(C_) #383.4619060554966
-
-regressor = sklearn.svm.LinearSVR(epsilon=0.0, tol=0.0001, C=0.001, loss='epsilon_insensitive',
-                                 fit_intercept=True, intercept_scaling=1.0, dual=True, verbose=0,
-                                random_state=999, max_iter=5000)
+# print(C_) #0.001
+regressor = sklearn.svm.LinearSVR(epsilon=0.0, tol=0.1, C=0.1, loss='epsilon_insensitive',
+                                  fit_intercept=True, intercept_scaling=1.0, dual=True, verbose=0,
+                                 random_state=999, max_iter=5000)
+# Complete data
+# Without log : Part A: C = 0.1, MSE = 0.87, R2 = 0.40  Part B: C = 0.1, MSE = 0.85, R2 = 0.40
+# With log : Part A: C = 0.001, MSE = 0.94, R2 = 0.51  Part B: C = 0.001, MSE = 0.93, R2 = 0.53
+# Baseline
+# Without log : Part A: C = 0.1, MSE = 3.01, R2 = 0.1  Part B: C = 0.1, MSE = 2.81, R2 = 0.16
+# With log : Part A: C = 0.001, MSE = 3.14, R2 = 0.1  Part B: C = 0.001, MSE = 3.04, R2 = 0.14
 
 
 ############################################################################
@@ -324,6 +349,7 @@ result = cross_validate(regressor,X=text_fields,y=y_train,cv=5,scoring={'mse':sc
 print("Overall MSE: %f" % np.mean(result['test_mse']))
 print("R2: %f" % np.mean(result['test_r2']))
 print(result['test_r2'])
+
 
 # from prettytable import PrettyTable
 # print("\n BOW features")
@@ -338,15 +364,12 @@ print(result['test_r2'])
 ######### Generating Diagnostic Plots for the data #################
 
 #y_train = train['num_votes'].apply(lambda x: np.log(x))
-
-#y_train = train['num_votes']
-
-# print(type(text_fields))
-# text_fields = scipy.sparse.hstack([summary,source,location],format='csr')
-# rows = int(0.2 * (text_fields.get_shape()[0]))
+# y_train = train['num_votes']
+#
+# rows = int(0.01 * (text_fields.get_shape()[0]))
 # text_fields = text_fields[0:rows,:]
 # text_fields = text_fields.toarray()
-#diagnostic_plots(text_fields,y_train[0:rows])
-#plt.show()
+# diagnostic_plots(text_fields,y_train[0:rows])
+# plt.show()
 
 
--- lstm.py
+++ lstm.py
@@ -1,60 +1,3 @@
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-
-import os
-import dill
-import gensim
-import pickle
-import numpy as np
-import pandas as pd
-from keras.models import Model
-from keras.layers import Input, Dense, Embedding, SpatialDropout1D, Dropout, add, concatenate
-from keras.layers import Bidirectional, GlobalMaxPooling1D, GlobalAveragePooling1D, CuDNNLSTM, TimeDistributed, LSTM
-from keras.preprocessing import text, sequence
-from keras.callbacks import LearningRateScheduler
-from sklearn.metrics import mean_squared_error
-import sklearn
-import gensim
-import keras
-from gensim.models.word2vec import Word2Vec
-from gensim.models import KeyedVectors
-import gc
-import pickle
-import gensim.downloader as api
-import random
-from sklearn.externals import joblib
-from collections import defaultdict
-import dill
-import copy
-import json
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn import svm
-from nltk.tokenize import TweetTokenizer
-from sklearn.model_selection import cross_validate
-from math import sqrt
-
-
-def rmsle(h, y):
-    """
-   Compute the Root Mean Squared Log Error for hypthesis h and targets y
-
-   Args:
-       h - numpy array containing predictions with shape (n_samples, n_targets)
-       y - numpy array containing targets with shape (n_samples, n_targets)
-   """
-    return np.sqrt(np.square(np.log(h + 1) - np.log(y + 1)).mean())
-
-
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in
-
-train = pd.read_csv('train.csv', sep=',')
 # This Python 3 environment comes with many helpful analytics libraries installed
 # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to load in
@@ -66,7 +9,7 @@ EMBEDDING_FILES = [
 ]
 
 NUM_MODELS = 1
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 LSTM_UNITS = 200
 DENSE_HIDDEN_UNITS = 2 * LSTM_UNITS
 EPOCHS = 10
@@ -94,39 +37,50 @@ def build_matrix(word_index, embedding, size):
     return embedding_matrix
 
 
-def build_model(embedding_matrix, source_shape, location_shape):
+def build_model(embedding_matrix, one_hot_shape):
     words = Input(shape=(MAX_LEN,))
     x = Embedding(*embedding_matrix.shape, weights=[embedding_matrix], trainable=False)(words)
+    x = Bidirectional(CuDNNLSTM(LSTM_UNITS, return_sequences=True), merge_mode='concat')(x)
+    x = SpatialDropout1D(rate=0.3)(x)
     x = Bidirectional(CuDNNLSTM(LSTM_UNITS, return_sequences=True), merge_mode='ave')(x)
-    x = Bidirectional(CuDNNLSTM(LSTM_UNITS, return_sequences=True), merge_mode='ave')(x)
-    # x = SpatialDropout1D(rate=0.3)(x)
+    x = SpatialDropout1D(rate=0.3)(x)
 
-    hidden = GlobalAveragePooling1D()(x)  # this layer average each output from the Bidirectional layer
+    # x = GlobalAveragePooling1D()(x) # this layer average each output from the Bidirectional layer
 
-    summary = Input(shape=(20,))
+    x = concatenate([
+        GlobalMaxPooling1D()(x),
+        GlobalAveragePooling1D()(x),
+    ])
+
+    summary = Input(shape=(50,))
     x_aux = Embedding(*embedding_matrix.shape, weights=[embedding_matrix], trainable=False)(summary)
+    x_aux = Bidirectional(CuDNNLSTM(LSTM_UNITS, return_sequences=True), merge_mode='concat')(x_aux)
+    x_aux = SpatialDropout1D(rate=0.3)(x_aux)
     x_aux = Bidirectional(CuDNNLSTM(LSTM_UNITS, return_sequences=True), merge_mode='ave')(x_aux)
-    x_aux = GlobalAveragePooling1D()(x_aux)
+    x_aux = SpatialDropout1D(rate=0.3)(x_aux)
 
-    source = Input(shape=(source_shape,))
-    location = Input(shape=(location_shape,))
-    hidden = concatenate([hidden, x_aux, source, location])
-    # hidden = concatenate([
-    #    GlobalMaxPooling1D()(x),
-    #    GlobalAveragePooling1D()(x),
-    # ])
-    hidden = Dense(300, activation='relu')(hidden)
-    hidden = Dropout(0.3)(hidden)
-    # hidden = add([hidden,Dense(DENSE_HIDDEN_UNITS, activation='relu')(hidden)])
-    # hidden = add([hidden, Dense(DENSE_HIDDEN_UNITS, activation='relu')(hidden)])
+    # x_aux = GlobalAveragePooling1D()(x_aux)
+    x_aux = concatenate([
+        GlobalMaxPooling1D()(x_aux),
+        GlobalAveragePooling1D()(x_aux),
+    ])
+
+    one_hot = Input(shape=(one_hot_shape,))
+    hidden = concatenate([x, x_aux, one_hot])
+
     hidden = Dense(400, activation='relu')(hidden)
-    hidden = Dropout(0.3)(hidden)
+    hidden = Dropout(0.4)(hidden)
+    hidden = Dense(400, activation='relu')(hidden)
+    hidden = Dropout(0.4)(hidden)
+    hidden = Dense(300, activation='relu')(hidden)
+    hidden = Dropout(0.4)(hidden)
+    hidden = Dense(300, activation='relu')(hidden)
+    hidden = Dropout(0.4)(hidden)
     hidden = Dense(100, activation='relu')(hidden)
     result = Dense(1, activation='linear')(hidden)
-    # aux_result = Dense(num_aux_targets, activation='sigmoid')(hidden)
 
-    # model = Model(inputs=words, outputs=[result, aux_result])
-    model = Model(inputs=[words, summary, source, location], outputs=[result])
+    model = Model(inputs=[words, summary, one_hot], outputs=[result])
+    # adam = keras.optimizers.Adam(lr=0.0001, clipnorm=1.0, clipvalue=0.5)
     model.compile(loss='mse', optimizer='adam')
 
     return model
@@ -147,47 +101,64 @@ def preprocess(data):
     return data
 
 
+# train total
 train = pd.read_csv('../input/see-click-predict-fix/train.csv', sep=',')
-train['description'].fillna(' ', inplace=True)
+train = train[(train['num_votes'] < 50)]
+train['description'].fillna('this is a test', inplace=True)
 # train = train[train['created_time'] > '2013-01-01 00:00:00']
-train = train.dropna(subset=['source'])
+# train = train.dropna(subset=['source'])
+train.reset_index(inplace=True)
 print(train.info())
+
+# train reduced
+# train = train[(train['num_votes'] > 1) & (train['num_votes'] < 50)]
+
+# train baseline
+train_baseline = train[~train['tag_type'].isna()]
+# train = train_baseline
+# print(train.info())
 
 train.loc[:, 'description'] = train['description'].apply(lambda x: ' '.join(gensim.utils.simple_preprocess(x)))
 train.loc[:, 'summary'] = train['summary'].apply(lambda x: ' '.join(gensim.utils.simple_preprocess(x)))
 train.loc[:, 'latitude'] = train['latitude'].apply(lambda x: np.round(x, 3))
 train.loc[:, 'longitude'] = train['longitude'].apply(lambda x: np.round(x, 3))
 
+train['hour'] = [str(pd.to_datetime(x).hour) for x in train['created_time']]
+train['dayofweek'] = [str(pd.to_datetime(x).weekday()) for x in train['created_time']]
+train['year'] = [str(pd.to_datetime(x).year) for x in train['created_time']]
+
 tokenizer = text.Tokenizer()
 tokenizer.fit_on_texts(list(train['description']) + list(train['summary']))
 
-test = train.sample(frac=.2, random_state=999)
-train = train.iloc[~train.index.isin(test.index), :]
+# test = train.sample(frac=.2,random_state=999)
+# train =  train.iloc[~train.index.isin(test.index),:]
+
+# y_test = test['num_votes'].apply(lambda x: np.log1p(x+1))
+# y_train = train['num_votes'].apply(lambda x: np.log1p(x+1))
+# y_test = test['num_votes']
+y_train = train['num_votes']
 
 description_train = train['description']
-description_test = test['description']
-# x_train = preprocess(train['description'])
-# x_test = preprocess(test['description'])
+# description_test = test['description']
 description_train = tokenizer.texts_to_sequences(description_train)
-description_test = tokenizer.texts_to_sequences(description_test)
+# description_test = tokenizer.texts_to_sequences(description_test)
 description_train = sequence.pad_sequences(description_train, maxlen=MAX_LEN)
-description_test = sequence.pad_sequences(description_test, maxlen=MAX_LEN)
+# description_test = sequence.pad_sequences(description_test, maxlen=MAX_LEN)
 
 summary_train = train['summary']
-summary_test = test['summary']
-# input_aux_train = preprocess(input_aux_train)
-# input_aux_test = preprocess(input_aux_test)
+# summary_test = test['summary']
 summary_train = tokenizer.texts_to_sequences(summary_train)
-summary_test = tokenizer.texts_to_sequences(summary_test)
-summary_train = sequence.pad_sequences(summary_train, 20)
-summary_test = sequence.pad_sequences(summary_test, 20)
+# summary_test = tokenizer.texts_to_sequences(summary_test)
+summary_train = sequence.pad_sequences(summary_train, 50)
+# summary_test = sequence.pad_sequences(summary_test,50)
 
 from sklearn.preprocessing import OneHotEncoder
 
 dummies = OneHotEncoder(handle_unknown='ignore')
-source_train = dummies.fit_transform(np.array(train[['source']]).reshape(-1, 1)).todense()
+
+# source_train = dummies.fit_transform(np.array(train[['source']]).reshape(-1,1)).todense()
 # source = dummies.fit_transform(train[['source','num_votes']]) #this should be wrong but is issuing a smaller error
-source_test = dummies.transform(np.array(test[['source']]).reshape(-1, 1)).todense()
+# source_test = dummies.transform(np.array(test[['source']]).reshape(-1,1)).todense()
 
 # Decimal places      Object that can be unambiguously recognized at this scale
 # 0	                  country or large region
@@ -200,17 +171,18 @@ source_test = dummies.transform(np.array(test[['source']]).reshape(-1, 1)).toden
 # 7                   practical limit of commercial surveying
 # 8                   specialized surveying (e.g. tectonic plate mapping)
 
-latitude = train['latitude']
-longitude = train['longitude']
-from sklearn.preprocessing import StandardScaler
+latitude_train = train['latitude']
+longitude_train = train['longitude']
+# latitude_test = test['latitude']
+# longitude_test = test['longitude']
 
-scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
-location_train = scaler.fit_transform(np.stack((latitude, longitude), axis=-1))
-# location_train = scipy.sparse.csr_matrix(location_train)
-latitude = test['latitude']
-longitude = test['longitude']
-location_test = scaler.transform(np.stack((latitude, longitude), axis=-1))
-# location_test = scipy.sparse.csr_matrix(location_test)
+onehot_train = np.stack((train['hour'], train['dayofweek'], train['year'], latitude_train, longitude_train), axis=-1)
+onehot_train = dummies.fit_transform(onehot_train)
+# onehot_train = scipy.sparse.csr_matrix(onehot_train)
+
+# onehot_test = np.stack((test['hour'],test['dayofweek'],test['year'],latitude_test,longitude_test),axis=-1)
+# onehot_test = dummies.transform(onehot_test)
+# onehot_test = scipy.sparse.csr_matrix(onehot_test)
 
 
 embedding = KeyedVectors.load_word2vec_format("../input/glove2word2vec/glove_w2v.txt", binary=False)
@@ -218,48 +190,63 @@ embedding = KeyedVectors.load_word2vec_format("../input/glove2word2vec/glove_w2v
 EMBEDDINGS = [embedding]
 embedding_matrix = build_matrix(tokenizer.word_index, embedding, 200)
 del embedding
-
 gc.collect()
 
 
 # embedding_matrix = np.concatenate(
 #     [build_matrix(tokenizer.word_index, wordvect,200) for wordvect in EMBEDDINGS], axis=-1)
 
-def main():
+
+def main(x_train, y_train, one_hot_shape):
     checkpoint_predictions = []
     weights = []
 
+    model = build_model(embedding_matrix, one_hot_shape)
+
     for model_idx in range(NUM_MODELS):
-        model = build_model(embedding_matrix, source_shape=source_train.shape[1],
-                            location_shape=location_train.shape[1])
+        # model = build_model(embedding_matrix,one_hot_shape)
         for global_epoch in range(EPOCHS):
             model.fit(
-                [description_train, summary_train, source_train, location_train],
+                x_train,
                 # [Y_train, y_aux_train],
-                train['num_votes'],
+                y_train,
                 batch_size=BATCH_SIZE,
                 epochs=1,
                 verbose=2,
-                callbacks=[
-                    LearningRateScheduler(lambda epoch: 1e-3 * (0.6 ** global_epoch))
-                ]
+                # callbacks=[
+                #   LearningRateScheduler(lambda epoch: 1e-3 * (0.6 ** global_epoch))
+                # ]
             )
-            checkpoint_predictions.append(
-                model.predict([description_test, summary_test, source_test, location_test], batch_size=2048)[
-                    0].flatten())
-            weights.append(2 ** global_epoch)
+            # checkpoint_predictions.append(model.predict(x_test, batch_size=2048)[0].flatten())
+            # weights.append(2 ** global_epoch)
 
     model.save('lstm_model_3.h5')
-    # pickle.dump(model, open('lstm_model','wb'))
-
-    pred = model.predict([description_test, summary_test, source_test, location_test]).flatten()
-    pickle.dump(pred, open('prediction_3.save', 'wb'))
+    return model
 
 
-main()
-model = keras.models.load_model('lstm_model_3.h5')
-pred = pickle.load(open('prediction_3.save', 'rb'))
-auc = mean_squared_error(test['num_votes'], pred)
-print('Overall Test RMSE %.3f' % sqrt(auc))
-print('RMSLE %.3f' % rmsle(pred, test['num_votes'])) #RMSLE 0.145
+# This creates a matrix with all the train data. But is useless
+# description_train = np.array(description_train).reshape(len(description_train),MAX_LEN)
+# summary_train = np.array(summary_train).reshape(len(summary_train),50)
+# final_train = scipy.sparse.hstack([scipy.sparse.csr_matrix(description_train),scipy.sparse.csr_matrix(summary_train),onehot_train],format='csr')
+
+kfold = KFold(n_splits=2, shuffle=True, random_state=999)
+cvscores = []
+for train_idx, test_idx in kfold.split(onehot_train):
+    ## this is to guarantee full batches of BATCH_SIZE examples
+    # length_train = len(train_idx)%BATCH_SIZE
+    # length_train = len(train_idx) - length_train
+    # train_idx = train_idx[0:length_train]
+    # test_idx = test_idx[0:length_train]
+
+    test_idx = (train.sample(frac=.2)).index
+    train_idx = train[~train.index.isin(test_idx)].index
+
+    model = main([description_train[train_idx], summary_train[train_idx], onehot_train[train_idx]], y_train[train_idx],
+                 onehot_train.get_shape()[1])
+    pred = model.predict([description_train[test_idx], summary_train[test_idx], onehot_train[test_idx]]).flatten()
+    cvscores.append(np.mean(np.square(pred - y_train[test_idx])))
+    print('MSE %.3f' % np.mean(np.square(pred - y_train[test_idx])))
+
+print('Overall MSE: %.3f' % np.mean(cvscores))
+
 
